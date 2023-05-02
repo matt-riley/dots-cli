a6156a75d19f942777c562450536311455771252
Matt Riley <matt@mattriley.me>
2023-05-02 19:03:59 +0100

    refactor: add url validation on input prompt

--- go.mod
+++ go.mod
@@ -13,7 +13,6 @@ require (
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
--- internal/tui/InitPrompt.go
+++ internal/tui/InitPrompt.go
@@ -1,6 +1,8 @@
 package tui
 
 import (
+	"net/url"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/matt-riley/dots-cli/internal/style"
@@ -15,6 +17,7 @@ type InitPromptModel struct {
 	questions []InitQuestion
 	styles    *style.InitFormStyles
 	done      bool
+	validURL  bool
 }
 
 // InitQuestion is a question for the init prompt
@@ -22,6 +25,7 @@ type InitQuestion struct {
 	question string
 	answer   string
 	input    Input
+	url      bool
 }
 
 // Next increments the index of the init prompt
@@ -36,26 +40,33 @@ func newQuestion(q string) InitQuestion {
 	return InitQuestion{question: q}
 }
 
-func newShortQuestion(q string) InitQuestion {
+func newShortQuestion(q string, url bool) InitQuestion {
 	question := newQuestion(q)
 	model := NewShortAnswerField()
 	question.input = model
+	question.url = url
 	return question
 }
 
 // InitialInitModel returns an initial model for the init prompt
 func InitialInitModel(exists bool) InitPromptModel {
 	questions := []InitQuestion{
-		newShortQuestion("The URL of the dotfiles repo"),
-		newShortQuestion("The branch to use"),
-		newShortQuestion("Your Github PAT"),
+		newShortQuestion("The URL of the dotfiles repo", true),
+		newShortQuestion("The branch to use", false),
+		newShortQuestion("Your Github PAT", false),
 	}
 	styles := style.DefaultInitFormStyles()
 	mod := InitPromptModel{}
 	mod.exists = exists
 	mod.questions = questions
 	mod.styles = styles
+	mod.validURL = true
 	return mod
+}
+
+func validateURL(input string) bool {
+	_, err := url.ParseRequestURI(input)
+	return err == nil
 }
 
 // Init returns a command for the init prompt
@@ -73,17 +84,26 @@ func (m InitPromptModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.height = msg.Height
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "esc", "ctrl+c":
+		case "esc", "ctrl+c":
 			return m, tea.Quit
 		case "enter":
 			if m.done {
 				return m, tea.Quit
 			}
+			if currentQuestion.url {
+				valid := validateURL(currentQuestion.input.Value())
+				m.validURL = valid
+			} else {
+				m.validURL = true
+			}
 			if m.index == len(m.questions)-1 {
 				m.done = true
 			}
-			currentQuestion.answer = currentQuestion.input.Value()
-			m.Next()
+
+			if m.validURL {
+				currentQuestion.answer = currentQuestion.input.Value()
+				m.Next()
+			}
 			return m, currentQuestion.input.Blur
 		}
 	}
@@ -110,13 +130,35 @@ func (m InitPromptModel) View() string {
 		)
 	}
 	current := m.questions[m.index]
+
+	if !m.validURL {
+		return lipgloss.Place(
+			m.width,
+			m.height,
+			lipgloss.Center,
+			lipgloss.Center,
+			lipgloss.JoinVertical(
+				lipgloss.Left,
+				m.styles.Question.Render(
+					m.questions[m.index].question,
+				)+" "+lipgloss.NewStyle().
+					Bold(true).
+					Foreground(style.Red).
+					Render("Please amend input to be a valid URL"),
+				m.styles.InputField.BorderForeground(style.Red).
+					Render(current.input.View()),
+			),
+		)
+	}
+
 	return lipgloss.Place(
 		m.width, m.height,
 		lipgloss.Center, lipgloss.Center,
 		lipgloss.JoinVertical(
 			lipgloss.Left,
 			m.styles.Question.Render(m.questions[m.index].question),
-			m.styles.InputField.Render(current.input.View()),
+			m.styles.InputField.BorderForeground(style.Green).
+				Render(current.input.View()),
 		),
 	)
 }
